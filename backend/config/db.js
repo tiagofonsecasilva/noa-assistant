d2b4dac110a0113414966850667f9a3d60aef44a
Tiago Fonseca da Silva <tiagoseixasfonseca@gmail.com>
2021-07-20 16:58:06 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         //database Name
         const databaseName='mern';
-        const con = await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`, { 
+        const con = await mongoose.connect(`mongodb+srv://ironhack:admin@cluster0.1avs1.mongodb.net/${databaseName}?retryWrites=true&w=majority`, { 
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
