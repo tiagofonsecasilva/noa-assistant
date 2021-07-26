f6806642c385f2aa0e1f82d97b9c23611fdff8c4
whitesock23 <tiagomiguelseixas@me.com>
2021-07-26 21:00:41 +0100

    Search navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,20 @@ import GitPullRequest from './components/GitHub';
 import withAuthenticationRequired from './components/Auth0/withAuthenticationRequired';
 import { Container, Row, Col } from 'reactstrap';
 import LeftBar from './components/LeftBar';
+import { SearchBar } from './components/searchBar';
+import styled from "styled-components";
 
 
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  position: fixed; /* or absolute */
+  top: 40%;
+  left: 10%;
+`;
 
 const App = () => {
   const [users, setUsers] = useState([])
@@ -22,7 +34,11 @@ const App = () => {
 
   return (
     <>
+          <AppContainer>
+      <SearchBar />
+    </AppContainer>
       <LeftBar />
+
     </>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,17 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-  <Auth0Provider
-        domain="dev-jrdvq77o.eu.auth0.com"
-        clientId="bFcmZVjRbaCvHMy3yOTYOa8Cks7whpzQ"
-        redirectUri={window.location.origin}
+    <Auth0Provider
+      domain="dev-jrdvq77o.eu.auth0.com"
+      clientId="bFcmZVjRbaCvHMy3yOTYOa8Cks7whpzQ"
+      redirectUri={window.location.origin}
     >
-    <App />
+      <Router>
+        <App />
+      </Router>
     </Auth0Provider>,
   </React.StrictMode>,
   document.getElementById('root')
